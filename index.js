1e0926e36c8ec7f81c51ef83b807a275fe78b6b7
Jorge Paris Junior <mrjorgeparis@gmail.com>
2025-01-08 19:35:36 +0100

    Primeira vez usando Fetch para consumir 'API', entre parentes porque o ficheiro JSON eh LOCAL

--- index.js
+++ index.js
@@ -1,10 +1,44 @@
+const producto = document.querySelector(".products-grid");
+const mobile=document.querySelector('.product-container-mobile')
+const json = "./data.json";
 
+fetch(json)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.map((product) => {
+      const { image, name, category, price } = product;
+      producto.innerHTML += `
+<div class="product">
+<img
+src="${image.desktop}"
+alt=""
+class="product-img"
+/>
+<a class="cart-container" href="#">
+<img src="./assets/images/icon-add-to-cart.svg" alt="" class="cart-img" />
+<h5 class="add-to-cart-text">Add to cart</h5>
+</a>
+<div class="product-cat-text">
+<h5 class="product-type">${category}</h5>
+<h5>${name}</h5>
+<h5 class="product-price">${price}</h5>
+</div>
+</div>`;
 
-const carts=document.querySelectorAll('.cart-container')
-const modal=document.querySelector('.modal')
+mobile.innerHTML+=` <div class="product-mobile">
+          <img src="${image.mobile}" alt="">
+          <a href="" class="cart-container-mobile"> <img src="./assets/images/icon-add-to-cart.svg" alt="">
+          <h5>Add to Cart</h5>
+          </a>
+          <div class="mobile-text-container">
 
-carts.forEach(cart => {
-    cart.addEventListener('click',()=>{
-       modal.showModal()
-    })
-});
+            <h5 class="product-mobile-text"> ${category}</h5>
+            <h5 class="product-mobile-text"> ${name}</h5>
+            <h5 class="product-mobile-text"> ${price}</h5>
+          </div>
+         
+        </div>`
+    });
+  });
