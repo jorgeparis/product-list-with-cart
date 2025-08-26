5d78dbfac3ae005c2c2bb73021dfb2cb70120dca
Jorge Paris <mrjorgeparis@gmail.com>
2025-08-26 12:05:27 +0200

    sem ideia da mensage,

--- route-pr/src/App.jsx
+++ route-pr/src/App.jsx
@@ -1,14 +1,18 @@
+import "tailwindcss";
+import "./App.css";
 export default function App() {
   return (
     <>
-    <div className="containr">
-      <ul className="lista">
-        <li>item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-      </ul>
-    </div>
+      <div className="containr">
+        <ul className="lista">
+          <li text-3xl font-bold underline>
+            item 1
+          </li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+          <li>Item 4</li>
+        </ul>
+      </div>
     </>
   );
 }
