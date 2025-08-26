a4134c4585bf5c253758a443f2d7bc016233bed1
Jorge Paris <mrjorgeparis@gmail.com>
2025-08-26 13:28:18 +0200

    ndsds

--- route-pr/src/App.jsx
+++ route-pr/src/App.jsx
@@ -1,16 +1,25 @@
 import "./App.css";
 const message = "Hello Jorge";
 
+import { useState } from "react";
+
+const [mgs, setMessage] = useState("OLA JORGE");
 const App = (d) => {
   return (
     <>
       <div className="containr">
         <h1 class="text-sm font-semibold tracking-wide text-indigo-500 uppercase">
-          Hello world!
+          {mgs}
         </h1>
-        <h1 class="text-sm font-semibold">
-          Hello world!
-        </h1>
+        <h1 class="text-sm font-semibold">Hello world!</h1>
+        <button
+          onClick={() => {
+            setMessage("Hello Jorge");
+          }}
+        >
+          {" "}
+          Click me
+        </button>
       </div>
     </>
   );
