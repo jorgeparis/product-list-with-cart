a1e6fb92035c0126e37c1a771a88992a6da6cf6f
Jorge Paris <mrjorgeparis@gmail.com>
2025-08-26 11:40:43 +0200

    sem ideia da mensage,

--- route-pr/src/App.jsx
+++ route-pr/src/App.jsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <>
+    <div className="containr">
+      <ul className="lista">
+        <li>item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+      </ul>
     </div>
+    </>
   );
 }
