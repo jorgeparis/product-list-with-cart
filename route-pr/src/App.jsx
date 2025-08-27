26cf0097d954cea45434593e2a1f5ea8d6b8824b
Jorge Paris <mrjorgeparis@gmail.com>
2025-08-27 10:51:36 +0200

    Nada novo

--- route-pr/src/App.jsx
+++ route-pr/src/App.jsx
@@ -1,15 +1,41 @@
-import { Camera } from "lucide-react";
+import { Camera, Funnel } from "lucide-react";
 import "./App.css";
 const message = "Hello Jorge";
 
+import { ChevronRight } from "lucide-react";
 import { useState } from "react";
 
 const App = (d) => {
   const [mg, setMg] = useState("OLA");
   return (
     <>
-      <div className="containr">
-        <Camera color="black" size={20} />;
+      <div className="containter flex mt-px items-center justify-center backdrop-grayscale-25 w-full h-full">
+        <ul>
+          <li className="list-item">
+            {" "}
+            <Camera color="black" size={20} />
+            <Funnel color="blue" size={20} />
+            <ChevronRight />
+          </li>
+          <li className="list-item">
+            {" "}
+            <Camera color="black" size={20} />
+            <Funnel />
+            <ChevronRight />
+          </li>
+          <li className="list-item">
+            {" "}
+            <Camera color="#000000" size={20} />
+            <Funnel />
+            <ChevronRight />
+          </li>
+          <li className="list-item">
+            {" "}
+            <Camera color="black" size={20} />
+            <Funnel />
+            <ChevronRight />
+          </li>
+        </ul>
       </div>
     </>
   );
