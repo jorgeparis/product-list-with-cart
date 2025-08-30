ecfdc218ebb3199ab0f261cb18e28af305322742
Jorge Paris <mrjorgeparis@gmail.com>
2025-08-30 16:40:53 +0200

    Background Applied

--- route-pr/src/components/Navbar/Navbar.jsx
+++ route-pr/src/components/Navbar/Navbar.jsx
@@ -13,32 +13,56 @@ function Navbar() {
     alert("OLA");
   };
   return (
-    <div className="navbar w-screen h-[150px] bg-[#e4e2e2] flex gap-[20rem] items-center justify-center sticky top-0 z-99">
+    <div className="navbar min-w-[100vw] min-h-[10rem] bg-linear-65 from-purple-500 to-pink-500 flex gap-[20rem] items-center justify-center sticky top-0 z-99 shadow">
       <div className="navbar-container">
-        <ul className="text-[#222020] font-bold list-none flex items-center gap-4 cursor-pointer">
+        <ul className="text-[#ffffff] font-bold list-none flex items-center gap-4 cursor-pointer">
           <li
-            className="hover:text-[#f57591] hover:text-3xl hover:gap-[20rem]"
+            className="hover:text-[#000000] hover:text-3xl hover:gap-[20rem]"
             onClick={HandListItem}
           >
-            Stations
+            <a
+              href="#"
+              className=""
+              onClick={(event) => {
+                event.preventDefault();
+              }}
+            >
+              Stations
+            </a>
           </li>
           <li
-            className="hover:text-[#f57591] hover:text-3xl"
+            className="hover:text-[#000000] hover:text-3xl"
             onClick={HandListItem}
           >
-            News
+            <a
+              href="#"
+              className=""
+              onClick={(event) => {
+                event.preventDefault();
+              }}
+            >
+              Stations
+            </a>
           </li>
           <li
-            className="hover:text-[#f57591] hover:text-3xl"
+            className="hover:text-[#000000] hover:text-3xl"
             onClick={HandListItem}
           >
-            About
+            <a
+              href="#"
+              className=""
+              onClick={(event) => {
+                event.preventDefault();
+              }}
+            >
+              Stations
+            </a>
           </li>
         </ul>
       </div>
       <div className="login-button text-[#222020] font-bold">
         <button
-          className="bg-[#222020] text-white rounded-[1rem] w-[6rem] h-[3rem] cursor-pointer"
+          className="bg-[#222020] text-white rounded-[1rem] w-[6rem] h-[3rem] hover:w-[7rem] hover:h-[3rem] cursor-pointer hover:bg-amber-200 hover:text-[#000] hover:font-extrabold"
           onClick={HandleSignIn}
         >
           Login
