bf47a82fadc779fc1ef5350cde75bd8ba4566756
Jorge Paris <mrjorgeparis@gmail.com>
2025-09-08 22:15:43 +0200

    ALGUMAS NOVAS COISAS

--- FRONTEND-REACTJS/src/components/Player/Player.jsx
+++ FRONTEND-REACTJS/src/components/Player/Player.jsx
@@ -1,6 +1,5 @@
 import { Play, SkipBack, SkipForward, Square } from "lucide-react";
 import React, { useRef, useState } from "react";
-import Background from "../../assets/background-3.jpg";
 import "./Player.css";
 
 const Player = (props) => {
@@ -9,13 +8,13 @@ const Player = (props) => {
 
   const play = () => {
     audioRef.current.play();
-    audioRef.current.volume = 1.0; // Ensure volume is reset to full when playing
+    audioRef.current.volume = 1.0;
   };
 
   const stop = () => {
-    const fadeOutInterval = 50; // Interval in ms for volume reduction
-    const fadeOutSteps = 20; // Number of steps for fade-out
-    const volumeStep = audioRef.current.volume / fadeOutSteps; // Volume reduction per step
+    const fadeOutInterval = 50;
+    const fadeOutSteps = 20;
+    const volumeStep = audioRef.current.volume / fadeOutSteps;
 
     const fadeOut = () => {
       if (audioRef.current.volume > 0.05) {
@@ -34,14 +33,14 @@ const Player = (props) => {
   };
   return (
     <div className="player-container p-5 min-w-auto min-h-auto flex items-center gap-4">
-      <div className="radio-logo-container max-h-[100px] w-[180px]">
+      <div className="radio-logo-container max-h-[80px] w-[80px] rounded-full">
         <img
-          src={Background}
-          alt=""
-          className="logo rounded-4xl hover:scale-106"
+          src={props.background}
+          alt="LOGO"
+          className="logo rounded-4xl hover:scale-106 max-h-[80px] w-[80px] rounded-full bg-cover"
         />
       </div>
-      <div className="container-radio flex flex-col gap-3 items-start w-[800px] max-h-[100px] bg-[#e0d8d8] px-4 py-4 rounded-lg shadow backdrop-blur-3xl">
+      <div className="container-radio flex flex-col gap-3 items-start w-[800px] max-h-[100px] bg-[#fcf3f3] px-4 py-4 rounded-lg shadow backdrop-blur-5xl">
         <div className="radio-id flex gap-8 items-center justify-between max-w-[800px] h-[100px]">
           <h2 className="radio-id text-xl font-light text-[#072c29] hover:text-[#5ca2f1]">
             {props.radioId}
@@ -50,9 +49,11 @@ const Player = (props) => {
           {isPlaying ? (
             <a
               href="#"
-              className="onair bg-[#f70707] text-white rounded-4xl px-4 py-2 font-bold text-center"
+              className="onair bg-[#d00000] text-white rounded-4xl px-4 py-2 font-bold text-center"
               onClick={(event) => {
                 event.preventDefault();
+                audioRef.current.pause();
+                setIsPlaying(!isPlaying);
               }}
             >
               Live
@@ -63,6 +64,8 @@ const Player = (props) => {
               className="onair bg-[#000000] text-white rounded-4xl px-4 py-2 font-bold text-center"
               onClick={(event) => {
                 event.preventDefault();
+                audioRef.current.play();
+                setIsPlaying(!isPlaying);
               }}
             >
               Offline
@@ -74,10 +77,10 @@ const Player = (props) => {
           </audio>
         </div>
         <div className="player-controls flex gap-1.5 items-start">
-          <SkipBack className="text-[#e93407] hover:text-[##e93407]/40 shadow backdrop-blur-lg font-light" />
+          <SkipBack className="text-[#ff0054] hover:text-[##e93407]/40 font-light" />
           {isPlaying ? (
             <Square
-              className="text-[#e93407] hover:text-[##e93407]/40 font-light"
+              className="text-[#ff0054] hover:text-[##e93407]/40 font-light"
               onClick={() => {
                 stop();
                 setIsPlaying(!isPlaying);
@@ -85,14 +88,14 @@ const Player = (props) => {
             />
           ) : (
             <Play
-              className="text-[#e93407] hover:text-[##e93407]/40"
+              className="text-[#ff0054] hover:text-[#00a6fb]/40"
               onClick={() => {
                 play();
                 setIsPlaying(!isPlaying);
               }}
             />
           )}
-          <SkipForward className="text-[#e93407] hover:text-[##e93407]/40" />
+          <SkipForward className="text-[#ff0054] hover:text-[##e93407]/40" />
         </div>
       </div>
     </div>
--- FRONTEND-REACTJS/src/pages/Home/Home.jsx
+++ FRONTEND-REACTJS/src/pages/Home/Home.jsx
@@ -1,15 +1,74 @@
 import { useState } from "react";
+import Bk11 from "../../assets/background-11.png";
+import Bk1 from "../../assets/background-6.png";
+import Bk7 from "../../assets/background-7.jpg";
+import Bk8 from "../../assets/background-8.png";
+import Bk9 from "../../assets/background-9.jpg";
+import Animated from "../../assets/animated-background.gif"
 import Player from "../../components/Player/Player";
 import "./Home.css";
+
 const Home = () => {
   const [isOnair, setOnair] = useState(false);
+  const [activeRadioId, setActiveRadioId] = useState(null);
+
+  const radioStations = [
+    {
+      radioId: "Radio Maria 103.1 MHz",
+      src: "https://dreamsiteradiocp2.com/proxy/rmmozambique2?mp=/stream",
+      background: Bk1
+    },
+    {
+      radioId: "RM ANTENA NACIONAL",
+      src: "https://node.stream-africa.com:8443/AntenaNacional",
+      background: Bk8
+    },
+    {
+      radioId: "Radio Maria Papua New Guinea",
+      src: "https://dreamsiteradiocp2.com/proxy/rmpapua2?mp=/stream",
+      background: Bk1
+    },
+    {
+      radioId: "Radio Miramar",
+      src: "https://nl.digitalrm.pt:8150/stream",
+      background: Animated
+    },
+    {
+      radioId: "SUPER RM",
+      src: "https://c1.mirror.africa:8443/227",
+      background: Bk11
+    },
+
+    {
+      radioId: "Radio Mocambique-GAZA",
+      src: "https://node.stream-africa.com:8443/Gaza",
+      background: Bk8
+    },
+    {
+      radioId: "LM RADIO",
+      src: "https://cast6.asurahosting.com/proxy/lmradioc/stream",
+      background: Bk7
+    }
+  ];
+
+  const sortedStations = activeRadioId
+    ? [
+        ...radioStations.filter((station) => station.radioId === activeRadioId),
+        ...radioStations.filter((station) => station.radioId !== activeRadioId)
+      ]
+    : radioStations;
+
+  const handlePlay = (radioId) => {
+    console.log(`Playing: ${radioId}`);
+    setActiveRadioId(radioId);
+  };
+
   return (
     <div className="container-home min-w-screen min-h-screen bg-[#030916] flex flex-col items-center justify-center gap-6">
       <div
         className="hero-container min-w-screen min-h-[550px] flex flex-col justify-center bg-cover bg-center 
         bg-[linear-gradient(to_right,#66FCF1,#66fcf1),url('../../assets/background-1.jpg')]
-        bg-blend-multiply items-start px-[100px] gap-9
-      "
+        bg-blend-multiply items-start px-[100px] gap-9"
       >
         <h2 className="text-center text-[#66FCF1] font-bold text-2xl">
           YOUR BEST RADIO STATIONS
@@ -27,48 +86,38 @@ const Home = () => {
           href="#"
           className="text-center flex items-center justify-center 
           btn-sponsor bg-[#f10a1eef] hover:text-[#feffff]
-        hover:bg-[#f10a1eef]/70 h-[3rem] w-[10rem] 
+          hover:bg-[#f10a1eef]/70 h-[3rem] w-[10rem] 
           rounded-lg text-[#fff]
           max-lg:hidden py-2"
-          onClick={() => {
+          onClick={(e) => {
+            e.preventDefault();
             setOnair(!isOnair);
           }}
         >
-          SHOW STATIONS
+          {isOnair ? "HIDE STATIONS" : "SHOW STATIONS"}
         </a>
       </div>
       <div
-        className={` ${
+        className={`player-container ${
           isOnair
-            ? "player-container min-h-auto max-w-[50%] bg-[#f8ecec] rounded-lg flex flex-col gap-1"
+            ? "min-h-auto max-w-[50%] bg-[#f8ecec] rounded-lg flex flex-col gap-1"
             : "hidden"
         }`}
       >
-        <Player
-          radioId={"Radio Maria 103.1 MHz"}
-          src="https://dreamsiteradiocp2.com/proxy/rmmozambique2?mp=/stream"
-        />
-        <Player
-          radioId={"RM ANTENA NACIONAL"}
-          src="https://node.stream-africa.com:8443/AntenaNacional"
-        />
-        <Player
-          radioId={"Radio Maria Papua New Guinea"}
-          src="https://dreamsiteradiocp2.com/proxy/rmpapua2?mp=/stream"
-        />
-        <Player
-          radioId={"Radio Miramar"}
-          src="https://nl.digitalrm.pt:8150/stream"
-        />
-        <Player radioId={"SUPER RM"} src="https://c1.mirror.africa:8443/227" />
-        <Player
-          radioId={"Radio Mocambique-GAZA"}
-          src="https://node.stream-africa.com:8443/Gaza"
-        />
-        <Player
-          radioId={"LM RADIO"}
-          src="https://cast6.asurahosting.com/proxy/lmradioc/stream"
-        />
+        {sortedStations.length > 0 ? (
+          sortedStations.map((station) => (
+            <Player
+              key={station.radioId}
+              radioId={station.radioId}
+              src={station.src}
+              background={station.background}
+              onPlay={() => handlePlay(station.radioId)}
+              isPlaying={activeRadioId === station.radioId}
+            />
+          ))
+        ) : (
+          <p>No stations available</p>
+        )}
       </div>
       <div className="player min-h-[600px] min-w-[50%] bg-[#da0707] flex flex-col justify-center items-center rounded-lg"></div>
     </div>
